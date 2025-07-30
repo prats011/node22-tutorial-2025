cc471807b7c13fac4ed4e89e1c62cdd6ed2a9001
prats011 <jhaveripratham2006@gmail.com>
2025-07-30 22:29:44 +0700

    +home routes

--- server.js
+++ server.js
@@ -2,4 +2,14 @@ const express = require('express')
 const app = express()
 const PORT = 4999
 
+
+// GET POST PATCH PUT DELETE 
+app.get('/', (req,res) => {
+    console.log("You have reached the home route: GET ")
+})
+
+app.delete('/', (req,res) => {
+    console.log('What do you want to delete?')
+})
+
 app.listen(PORT, () => console.log(`Server has started on port: ${PORT}`))
