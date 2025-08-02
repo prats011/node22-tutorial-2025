f17caf25a3a518dd16e321ab31fbd19069614b19
prats011 <jhaveripratham2006@gmail.com>
2025-08-02 16:18:39 +0700

    +put Update and put Create

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(express.static('public'))
 app.use(require('cors')())
 app.use(mw)
 
-function mw(req, res, next){
+function mw(req, res, next) {
     console.log('Hit the MIDDLEWARE')
-    const {id} = req.query
-    if(id != 8){
+    const { id } = req.query
+    if (id != 8) {
         return res.sendStatus(403)
     }
     next()
@@ -24,8 +24,8 @@ function mw(req, res, next){
 const db = []
 
 //SCHEDULER
-function cron(ms, fn){
-    async function cb(){
+function cron(ms, fn) {
+    async function cb() {
         clearTimeout(timeout)
         await fn()
         timeout = setTimeout(cb, ms)
@@ -34,7 +34,7 @@ function cron(ms, fn){
     return () => { }
 }
 
-function consoleDB(){
+function consoleDB() {
     console.log('DB= ', db)
 }
 
@@ -43,36 +43,49 @@ cron(1000, consoleDB)
 //GET POST PATCH PUT DELETE 
 //This gets overrun by the html file when
 //  there is app.use(express.static('public'))
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     console.log("You have reached the home route: GET ")
-    res.status(200).send({"message": "Hi mom"})
+    res.status(200).send({ "message": "Hi mom" })
 })
 
 app.post('/api/info', (req, res) => {
-    const {information}  = req.body
+    const { information } = req.body
     console.log('The posted message: ', information)
-    console.log('hi')
     db.push(information)
     console.log('DB: ', db)
-    res.status(201).json({"yourMessage": information})
+    res.status(201).json({ "yourMessage": information })
 })
 
-app.put('/api', (req, res) => {
-    const {word} = req.query
-    const {information}  = req.body
+app.put('/api/create', (req, res) => {
+    const { information } = req.body
     db.push(information)
-    console.log('DB: ', db)
-    res.status(201).json({"yourMessage": information})
-    console.log(word)
-    res.sendStatus(200)
+    console.log('Added new item:', information)
+    return res.status(201).json({ "Added new item": information })
 })
 
-app.delete('/delete', (req,res) => {
+app.put('/api/update', (req, res) => {
+    const { information } = req.body
+    const { newInformation } = req.body
+    const index = db.findIndex(newInformation => newInformation == information)
+
+    db.splice(index, 1, newInformation)
+    console.log('Updated new information:', newInformation)
+    res.status(200).json({ "Updated new item": newInformation })
+
+})
 
 
-    //db.splice(0,1)
-    //console.log('DB: ', db)
-    //res.status(200).send('deleted:')
+app.delete('/delete', (req, res) => {
+    if (db.length > 0) {
+        const info = db[0]
+        db.splice(0, 1)
+        res.status(200).json({ "Deleted= ": info })
+        console.log('The deleted message: ', info)
+    } else {
+        console.log('Empty DB LIST')
+        res.status(404).send("List is Empty")
+    }
+
 
 })
 
